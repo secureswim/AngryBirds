404ebd0b28277d63f3dab594e6bace2aecece4d0
secureswim <siddharthatdmk@gmail.com>
2024-10-25 15:39:34 +0530

    LoadingState done.

--- core/src/main/java/com/ok/AngryBirds/States/LoadingState.java
+++ core/src/main/java/com/ok/AngryBirds/States/LoadingState.java
@@ -1,79 +1,57 @@
-//package com.ok.AngryBirds.States;
-//
-//import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.assets.AssetManager;
-//import com.badlogic.gdx.graphics.GL20;
-//import com.badlogic.gdx.graphics.Texture;
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-//
-//
-//public class LoadingState extends State{
-//    private final Texture background;
-//    private final Texture logo;
-//    private Texture progress_bar;
-//    private final AssetManager assetManager;
-//    private float progress;
-//    private float elapsed_time;
-//
-//
-//    protected LoadingState(GameStateManager gsm) {
-//        super(gsm);
-//        assetManager = new AssetManager();
-//        background=new Texture("loading_backgrounf.png");
-//        logo= new Texture("sgdf.png");
-//        assetManager.load("assets/other_asset.png", Texture.class);
-//
-//
-//    }
-//
-//    @Override
-//    public void show() {
-//        progress = 0;
-//        elapsed_time = 0;
-//    }
-//
-//
-//
-//    @Override
-//    protected void hande_input() {
-//    }
-//
-//    @Override
-//    public void update(float dt) {
-//        hande_input();
-//    }
-//
-//    @Override
-//    public void render(SpriteBatch sb) {
-//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//
-//        // Update elapsed time
-//        elapsed_time = delta;
-//
-//        // Calculate progress based on elapsed time (2 seconds duration)
-//        progress = Math.min(elapsedTime / 2.0f, 1.0f);
-//
-//        // Draw the loading bar
-//        batch.begin();
-//
-//        // Draw background of the loading bar
-//        batch.draw(backgroundTexture, 100, 100, 600, 50);
-//
-//        // Draw the progress bar fill
-//        batch.draw(progressBarTexture, 100, 100, 600 * progress, 50);
-//
-//        batch.end();
-//
-//        // Once progress reaches 1 (100%), check if asset loading is done
-//        if (progress >= 1 && assetManager.update()) {
-//            // All assets are loaded, proceed to the next screen or game logic
-//            // For example: game.setScreen(new MainGameScreen());
-//        }
-//    }
-//
-//    @Override
-//    public void dispose() {
-//        background.dispose();
-//        logo.dispose();
-//    }
-//}
+package com.ok.AngryBirds.States;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Timer;
+import com.ok.AngryBirds.Main;
+
+public class LoadingState extends State{
+
+    private final Texture loading_bg;
+    private final Texture loading_bar;
+    private final Texture loading_bar_background;
+    private final float loadingTime;
+    private float elapsedTime;
+
+    public LoadingState(GameStateManager gsm) {
+        super(gsm);
+        loading_bg=new Texture("loading_background.png");
+
+        loading_bar=new Texture("loading_bar.png");
+        loading_bar_background=new Texture("loading_bar_background.png");
+        loadingTime=3.5f;
+        elapsedTime=0f;
+    }
+
+    @Override
+    protected void hande_input() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        elapsedTime+=dt;
+
+        if (elapsedTime >=loadingTime) {
+            gsm.set(new MenuState(gsm));
+        }
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(loading_bg,0,0, Main.width,Main.height);
+        sb.draw(loading_bar_background,430,30,300,30);
+        float progress = Math.min(elapsedTime / loadingTime, 1f);
+        sb.draw(loading_bar,430,40,300*progress,20);
+
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        loading_bg.dispose();
+        loading_bar.dispose();
+        loading_bar_background.dispose();
+    }
+}
